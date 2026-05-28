c6ea372a44614463ba5241baef50048a2e234953
Axdiael <axtr_23@alu.uabcs.mx>
2026-05-27 19:54:07 -0700

    Cambio de color a texto de los input

--- app/src/main/java/com/aerosj/myapplication/ui/components/CustomInput.kt
+++ app/src/main/java/com/aerosj/myapplication/ui/components/CustomInput.kt
@@ -53,7 +53,7 @@ fun CustomInput(
         BasicTextField(
             value = value,
             onValueChange = onValueChange,
-            textStyle = TextStyle(color = textColor, fontSize = 16.sp),
+            textStyle = TextStyle(color = Color.Black, fontSize = 16.sp),
             visualTransformation = if (isPassword) PasswordVisualTransformation()
             else VisualTransformation.None,
 
@@ -71,7 +71,7 @@ fun CustomInput(
                     if (value.isEmpty()) {
                         Text(
                             text = placeholder,
-                            style = TextStyle(color = textColor, fontSize = 16.sp),
+                            style = TextStyle(color = Color.Black.copy(alpha = 0.6f), fontSize = 16.sp),
                             modifier = Modifier.padding(start = 12.dp)
                         )
                     }
